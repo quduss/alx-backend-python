50d6e4efaba1bf8f4fdacc17086a472943c89e50
quduss <shuaibqudus33@gmail.com>
2024-07-15 18:27:02 +0100

    Write a type-annotated function
    It takes a float n as argument and returns the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 2's module.
+'''
+import math
+
+
+def floor(n: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return math.floor(n)
