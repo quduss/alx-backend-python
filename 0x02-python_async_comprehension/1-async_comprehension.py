a35b97b1bb54801adcdb7ebecf96a67a32d8daf2
quduss <shuaibqudus33@gmail.com>
2024-07-19 23:05:08 +0100

    Use list comprehension to create list

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,7 +8,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """return list of 10 random numbers using an async comprehensing
     over async_generator"""
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [i async for i in async_generator()]
