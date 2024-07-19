f4b090294f95f0d7b3f21647a71976746e93253e
quduss <shuaibqudus33@gmail.com>
2024-07-19 22:14:10 +0100

    Annotate as Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Task 0"""
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """async_generator"""
     for _ in range(10):
         await asyncio.sleep(1)
